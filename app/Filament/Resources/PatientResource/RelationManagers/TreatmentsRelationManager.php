261901800c5998c53a309a6dd108593b5ce8e547
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-06-24 14:49:18 -0400

    enter relation manager

---
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filament\Resources\PatientResource\RelationManagers;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Actions\{BulkActionGroup, CreateAction, DeleteAction, DeleteBulkAction, EditAction};
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class TreatmentsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'treatments';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('description')
+            ->columns([
+                TextColumn::make('description'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
